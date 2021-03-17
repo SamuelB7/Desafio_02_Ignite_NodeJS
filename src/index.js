feaa733b3be95d78489f9fbb4e882532119df51c
Samuel Belo <belo.samuel@gmail.com>
2021-03-17 16:42:59 -0300

    Desafio concluído

--- src/index.js
+++ src/index.js
@@ -10,19 +10,53 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  const user = users.find(user => user.username === username)
+  if(!user) return response.status(404).json({error: "User not found"})
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+  if((user.pro == false && user.todos.length < 10) || user.pro == true) {
+    request.user = user
+    return next()
+  } else {
+    return response.status(403).json({error: "You can't create more todos!"})
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  const {id} = request.params
+
+  const user = users.find(user => user.username === username)
+  if(!user) return response.status(404)
+
+  const isUuid = validate(id)
+  if(isUuid == false) return response.status(400)
+
+  const todo = user.todos.find(todos => todos.id === id)
+  if(!todo) return response.status(404)
+
+  request.todo = todo
+  request.user = user
+
+  return next()  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  const user = users.find(user => user.id === id)
+  if(!user) return response.status(404)
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
